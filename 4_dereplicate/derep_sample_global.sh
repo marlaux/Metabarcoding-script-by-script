bb0914be2235d97878f6bdd1fe285f420e04e159
marlaux <marcelelaux@gmail.com>
2021-03-23 15:38:40 -0300

    Update derep_sample_global.sh

--- 4_dereplicate/derep_sample_global.sh
+++ 4_dereplicate/derep_sample_global.sh
@@ -6,18 +6,22 @@ module load StdEnv
 module load VSEARCH/2.13.4-iccifort-2019.1.144-GCC-8.2.0-2.31.1
 
 
-FINAL_FASTA="my_training_set_global_derep.fas"
+FINAL_FASTA="my_training_set_global_dp.fas"
 TMP_FASTA=$(mktemp)
 THREADS="4"
 
-for file in /cluster/projects/nn9623k/metapipe/3_read_cleaning/fastas/*_f1.fasta; do pre="$(basename $file _f1.fasta)"; echo $pre; vsearch --quiet	--threads ${THREADS} --derep_fulllength $file --sizeout --fasta_width 0 --relabel_sha1 --output ${pre}_dp.fasta 2>> ${pre}_dp.log; done;
+#Dereplicate by sample
+for file in /cluster/projects/nn9623k/metapipe/3_read_cleaning/fastas/*_f1.fasta; do pre="$(basename $file _f1.fasta)"; echo $pre; \
+	vsearch --quiet --threads ${THREADS}	\
+	--derep_fulllength $file	\
+	--sizeout	\ 
+	--fasta_width 0	\
+	--relabel_sha1	\ 
+	--output ${pre}_dp.fasta 2>> ${pre}_dp.log; done;
 
-# Global dereplication
-
-# Pool sequences
+#Global dereplication: pool sequences from samples
 cat cluster/projects/nn9623k/metapipe/3_read_cleaning/fastas/*.fasta > "${TMP_FASTA}"
 
-# Dereplicate (vsearch)
 vsearch --derep_fulllength "${TMP_FASTA}" \
              --threads ${THREADS}	\
 	     --sizein \
