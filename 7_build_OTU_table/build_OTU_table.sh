cca295fb6145e18e099dff6f0d6373b780b6cf07
marlaux <marcelelaux@gmail.com>
2021-03-23 15:56:02 -0300

    Update build_OTU_table.sh

--- 7_build_OTU_table/build_OTU_table.sh
+++ 7_build_OTU_table/build_OTU_table.sh
@@ -2,15 +2,15 @@
 
 module load Python/2.7.15-GCCcore-8.2.0
 
-FASTA="training_set_global_dp.fas"
+FASTA="my_training_set_global_dp.fas"
 SCRIPT="OTU_contingency_table.py"
 STATS="${FASTA/.fas/.stats}"
 SWARMS="${FASTA/.fas/.swarms}"
 REPRESENTATIVES="${FASTA/.fas/_1f_representatives.fas}"
 UCHIME="${FASTA/.fas/_1f_representatives.uchime}"
 ASSIGNMENTS="Blast_results_formatted2OTUtable.tab"
-QUALITY="my_samples_quality_file.qual"
-OTU_TABLE="training_set_OTU_table_complete.tab"
+QUALITY="my_training_set_quality_file.qual"
+OTU_TABLE="my_training_set_OTU_table_complete.tab"
 
 python \
     "${SCRIPT}" \
