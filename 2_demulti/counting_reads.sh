45cb6ec0c400daa57fb16c458167f4a6c3ad6ab4
marlaux <marcelelaux@gmail.com>
2021-03-23 17:23:26 -0300

    Update counting_reads.sh

--- 2_demulti/counting_reads.sh
+++ 2_demulti/counting_reads.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
+#UNCOMMENT FOR FQ OR FASTA
 for file in *.fq; do grep --with-filename -c "^@M" $file; done >> counting_demulti_reads.txt
+
+for file in *.fasta; do grep --with-filename -c "^>" $file; done >> counting_clean_reads.txt
+
