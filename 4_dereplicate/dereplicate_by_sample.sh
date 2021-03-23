f7723567f1019722e174fba94766b48926b4d786
marlaux <marcelelaux@gmail.com>
2021-03-23 15:39:37 -0300

    Update dereplicate_by_sample.sh

--- 4_dereplicate/dereplicate_by_sample.sh
+++ 4_dereplicate/dereplicate_by_sample.sh
@@ -8,7 +8,13 @@ module load VSEARCH/2.13.4-iccifort-2019.1.144-GCC-8.2.0-2.31.1
 
 THREADS="4"
 
-for file in /cluster/projects/nn9623k/metapipe/3_read_cleaning/fastas/*_f1.fasta; do pre="$(basename $file _f1.fasta)"; echo $pre; vsearch --quiet --threads ${THREADS} --derep_fulllength $file --sizeout --fasta_width 0 --relabel_sha1 --output ${pre}_dp.fasta 2>> ${pre}_dp.log; done;
+for file in /cluster/projects/nn9623k/metapipe/3_read_cleaning/fastas/*_f1.fasta; do pre="$(basename $file _f1.fasta)"; echo $pre;  \
+    vsearch --quiet --threads ${THREADS}  \
+    --derep_fulllength $file  \
+    --sizeout \
+    --fasta_width 0 \
+    --relabel_sha1  \
+    --output ${pre}_dp.fasta 2>> ${pre}_dp.log; done;
 
 mkdir derep_logs
 mv *.log derep_logs
