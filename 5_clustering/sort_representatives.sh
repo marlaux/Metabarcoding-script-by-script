78d84bcf09598638f5ef78e24b4968b263c9354b
marlaux <marcelelaux@gmail.com>
2021-03-23 15:42:25 -0300

    Update sort_representatives.sh

--- 5_clustering/sort_representatives.sh
+++ 5_clustering/sort_representatives.sh
@@ -5,7 +5,7 @@ module load StdEnv
 module load VSEARCH/2.13.4-iccifort-2019.1.144-GCC-8.2.0-2.31.1
 
 # Sort representatives
-FINAL_FASTA="my_samples_global_derep.fas"
+FINAL_FASTA="my_training_set_global_derep.fas"
 REPRESENTATIVES_1f=${FINAL_FASTA/.fas/_1f_representatives.fas}
 vsearch    \
         --fasta_width 0 \
