b65e081a60537268d31809814a0c1119c0ce4ea4
marlaux <marcelelaux@gmail.com>
2021-03-23 15:40:45 -0300

    Update chimera_checking.sh

--- 5_clustering/chimera_checking.sh
+++ 5_clustering/chimera_checking.sh
@@ -5,7 +5,7 @@ module load StdEnv
 module load VSEARCH/2.13.4-iccifort-2019.1.144-GCC-8.2.0-2.31.1
 
 # Chimera checking
-FINAL_FASTA="my_samples_global_derep.fas"
+FINAL_FASTA="my_training_set_global_dp.fas"
 REPRESENTATIVES=${FINAL_FASTA/.fas/_1f_representatives.fas}
 UCHIME=${REPRESENTATIVES/.fas/.uchime}
 vsearch    \
