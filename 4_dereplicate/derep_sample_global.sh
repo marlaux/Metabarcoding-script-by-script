86ceafc6e77d9f8c37d889d54f4ab90b9d7be132
marlaux <marcelelaux@gmail.com>
2021-03-24 13:11:07 -0300

    Update derep_sample_global.sh

--- 4_dereplicate/derep_sample_global.sh
+++ 4_dereplicate/derep_sample_global.sh
@@ -8,16 +8,14 @@ module load VSEARCH/2.13.4-iccifort-2019.1.144-GCC-8.2.0-2.31.1
 
 FINAL_FASTA="my_training_set_global_dp.fas"
 TMP_FASTA=$(mktemp)
-THREADS="4"
 
 #Dereplicate by sample
-for file in /cluster/work/users/marlaux/GITHUB/METAPIPE/3_read_cleaning/fastas_bash/*_f1.fasta; do pre="$(basename $file _f1.fasta)"; echo $pre; vsearch --quiet --threads ${THREADS} --derep_fulllength $file --sizeout --fasta_width 0 --relabel_sha1 --output ${pre}_dp.fasta 2>> ${pre}_dp.log; done;
+for file in /cluster/work/users/marlaux/GITHUB/METAPIPE/3_read_cleaning/fastas_bash/*_f1.fasta; do pre="$(basename $file _f1.fasta)"; echo $pre; vsearch --quiet --derep_fulllength $file --sizeout --fasta_width 0 --relabel_sha1 --output ${pre}_dp.fasta 2>> ${pre}_dp.log; done;
 
 #Global dereplication: pool sequences from samples
 cat cluster/projects/nn9623k/metapipe/3_read_cleaning/fastas/*.fasta > "${TMP_FASTA}"
 
 vsearch --derep_fulllength "${TMP_FASTA}" \
-             --threads ${THREADS}	\
 	     --sizein \
              --sizeout \
              --fasta_width 0 \
