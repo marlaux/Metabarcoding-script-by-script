20259d12e58b12959581d2684faf75b5be5ed8ff
marlaux <marcelelaux@gmail.com>
2021-03-28 12:43:27 -0300

    Update clustering_swarm_complete.sh

--- 5_clustering/clustering_swarm_complete.sh
+++ 5_clustering/clustering_swarm_complete.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 TMP_FASTA=$(mktemp --tmpdir=".")
-FINAL_FASTA="my_training_set_global_dp.fas"
+GLOBAL_DP="COI_Arthropoda_global_dp.fas"
 LENGTH=380 ###according to your expected amplicon lenght
 
 
@@ -10,14 +10,15 @@ module load StdEnv
 module load VSEARCH/2.13.4-iccifort-2019.1.144-GCC-8.2.0-2.31.1
 
 # Global dereplication
-cat /cluster/projects/nn9623k/metapipe/3_read_cleaning/dereplicated/3_read_cleaning/dereplicated/*.fasta > "${TMP_FASTA}"
+#cat /cluster/projects/nn9623k/metapipe/3_read_cleaning/dereplicated/3_read_cleaning/dereplicated/*.fasta > "${TMP_FASTA}"
+cat /cluster/work/users/marlaux/GITHUB/METAPIPE/3_read_cleaning/dereplicated_bash/*_dp.fasta > "${TMP_FASTA}"
 
 # Global dereplication
 vsearch --derep_fulllength "${TMP_FASTA}" \
         --sizein \
         --sizeout \
         --fasta_width 0 \
-        --output "${FINAL_FASTA}" > /dev/null
+        --output "${GLOBAL_DP}" > /dev/null
 
 rm -f "${TMP_FASTA}"
 
@@ -31,10 +32,10 @@ TMP_REPRESENTATIVES=$(mktemp --tmpdir=".")
 
 swarm   \
     -d 1 -f -t ${THREADS} -z \
-    -i ${FINAL_FASTA/.fas/.struct} \
-    -s ${FINAL_FASTA/.fas/.stats} \
+    -i ${GLOBAL_DP/.fas/.struct} \
+    -s ${GLOBAL_DP/.fas/.stats} \
     -w ${TMP_REPRESENTATIVES} \
-    -o ${FINAL_FASTA/.fas/.swarms} < ${FINAL_FASTA}
+    -o ${GLOBAL_DP/.fas/.swarms} < ${GLOBAL_DP}
 
 module --force purge
 module load StdEnv
@@ -44,12 +45,16 @@ module load VSEARCH/2.13.4-iccifort-2019.1.144-GCC-8.2.0-2.31.1
 vsearch \
         --fasta_width 0 \
         --sortbysize ${TMP_REPRESENTATIVES} \
-        --output ${FINAL_FASTA/.fas/_1f_representatives.fas}
+        --output ${GLOBAL_DP/.fas/_1f_representatives.fas}
 rm ${TMP_REPRESENTATIVES}
 
 # Chimera checking
-REPRESENTATIVES=${FINAL_FASTA/.fas/_1f_representatives.fas}
+REPRESENTATIVES=${GLOBAL_DP/.fas/_1f_representatives.fas}
 UCHIME=${REPRESENTATIVES/.fas/.uchime}
 vsearch         \
         --uchime_denovo "${REPRESENTATIVES}" \
         --uchimeout "${UCHIME}"
+
+FINAL=${REPRESENTATIVES/_1f_representatives.fas/_2f_representatives.fas}
+sed -e '/^>/ s/;size=/_/' -e '/^>/ s/;$//' "${REPRESENTATIVES}" > "${FINAL}"
+
