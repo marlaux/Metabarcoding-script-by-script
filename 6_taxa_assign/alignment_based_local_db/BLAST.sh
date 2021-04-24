1af63f419952ab7635647b10e100988bf4eab0bc
marlaux <marcelelaux@gmail.com>
2021-04-24 16:46:44 -0300

    Update BLAST.sh

--- 6_taxa_assign/alignment_based_local_db/BLAST.sh
+++ 6_taxa_assign/alignment_based_local_db/BLAST.sh
@@ -1,3 +1,4 @@
+
 module --force purge
 module load StdEnv
 module load BLAST+/2.10.1-iimpi-2020a
@@ -5,8 +6,8 @@ module load BLAST+/2.10.1-iimpi-2020a
 FASTA="COI_Arthropoda_global_dp_2f_representatives.fas"
 NCBI_DB="/cluster/shared/databases/blast/latest/nt"
 LOCAL_DB="Arthropoda_COI.uniq.fasta"
-SIMILARITY_INT="0.9"
-COVERAGE_INT="0.6"
+SIM="0.9"
+COV="0.6"
 HITS_PER_SUBJECT="1"
 EVALUE="0.001"
 THREADS="1"
@@ -17,13 +18,14 @@ TASK="megablast"
 #makeblastdb -in $LOCAL_DB -dbtype nucl -parse_seqids
 
 blastn -query $FASTA \
-  -task $TRY \
+  -task $TASK \
   -db $NCBI_DB \
   -num_threads $THREADS \
-  -perc_identity $SIMILARITY_INT  \
-  -qcov_hsp_perc $COVERAGE_INT  \
+  -perc_identity $SIM  \
+  -qcov_hsp_perc $COV  \
   -max_hsps $HITS_PER_SUBJECT \
   -max_target_seqs $HITS_PER_SUBJECT  \
   -parse_deflines \
   -evalue $EVALUE \
-  -outfmt "6 qseqid pident stitle sseqid sscinames" >  ${OUT}_blast.tab
+  -outfmt "6 qseqid pident stitle sseqid sscinames" >  ${OUT}_NT_blast.tab
+
