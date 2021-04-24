8e7ab051ffcce56a1f29697a3a355c1555b4c825
marlaux <marcelelaux@gmail.com>
2021-04-24 13:46:24 -0300

    Update BLAST.sh

--- 6_taxa_assign/alignment_based_local_db/BLAST.sh
+++ 6_taxa_assign/alignment_based_local_db/BLAST.sh
@@ -3,16 +3,16 @@ module load StdEnv
 module load BLAST+/2.10.1-iimpi-2020a
 
 FASTA="COI_Arthropoda_global_dp_2f_representatives.fas"
-NCBI_DB="/cluster/shared/databases/blast/25-10-2019/nt"
-#LOCAL_DB="Arthropoda_COI.uniq.fasta"
+NCBI_DB="/cluster/shared/databases/blast/latest/nt"
+LOCAL_DB="Arthropoda_COI.uniq.fasta"
 SIMILARITY_INT="0.9"
 COVERAGE_INT="0.6"
 HITS_PER_SUBJECT="1"
 EVALUE="0.001"
 THREADS="1"
-PREFIX_IN="COI"
-PREFIX_DB="Arthropoda"
-TRY="megablast"
+OUT="Arthropoda_COI"
+TASK="megablast"
+
 ###BLAST LOCAL DATABASE
 #makeblastdb -in $LOCAL_DB -dbtype nucl -parse_seqids
 
@@ -26,4 +26,4 @@ blastn -query $FASTA \
   -max_target_seqs $HITS_PER_SUBJECT  \
   -parse_deflines \
   -evalue $EVALUE \
-  -outfmt "6 qseqid pident stitle sseqid sscinames" >  ${PREFIX_IN}_${PREFIX_DB}_NT_blast.tab
+  -outfmt "6 qseqid pident stitle sseqid sscinames" >  ${OUT}_blast.tab
