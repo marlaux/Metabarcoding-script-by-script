02ce57ff97380c38c3fb13436dec08ab55be013a
marlaux <marcelelaux@gmail.com>
2021-08-11 18:28:34 -0300

    Update merge_pear.sh

--- 1_merge/merge_pear.sh
+++ 1_merge/merge_pear.sh
@@ -1,16 +1,15 @@
 #/bin/bash
-module --quiet purge
-module load StdEnv
-module load PEAR/0.9.11-GCCcore-9.3.0
-
-
+#module --quiet purge
+#module load StdEnv
+#module load PEAR/0.9.11-GCCcore-9.3.0
+#UNCOMMENT ONLY IF YOU ARE RUNNING IT IN BASH
 
 INPUT_F="training_set_R1_subsampled.fastq"
 INPUT_R="training_set_R2_subsampled.fastq"
 THREADS="4"
 PVALUE="0.001"  #Specify  a p-value for the statistical test
 OVERLAP="20"      #Specify the minimum overlap size.
-QUAL="20"      #uality  score  threshold for trimming the low quality 
+QUAL="20"      #uqality  score  threshold for trimming the low quality 
 UNCALLED="0"	#do not allow N's
 OUTPUT="my_training_set"
 
