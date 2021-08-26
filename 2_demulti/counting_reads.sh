6db26277bac278a25b38a910480ad3c707f230b7
marlaux <marcelelaux@gmail.com>
2021-08-26 13:42:26 -0300

    Delete counting_reads.sh

--- 2_demulti/counting_reads.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-#UNCOMMENT FOR FQ OR FASTA
-for file in *.fq; do grep --with-filename -c "^@M" $file; done >> counting_demulti_reads.txt
-
-for file in *.fasta; do grep --with-filename -c "^>" $file; done >> counting_clean_reads.txt
-
