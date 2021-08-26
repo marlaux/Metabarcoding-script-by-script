7be9c63d82cb7bd307c43cdb7f7d82261e0dc769
marlaux <marcelelaux@gmail.com>
2021-08-26 11:40:00 -0300

    Update tag_primer_clipping_wrap1.sh
    BAD output stored at adapter_clip/BAD and primer_clip/BAD for further checking, if needed.

--- 3_read_cleaning/tag_primer_clipping_wrap1.sh
+++ 3_read_cleaning/tag_primer_clipping_wrap1.sh
@@ -15,20 +15,21 @@ PRIMER_R1_RC="TGRTTYTTYGGNCAYCCHGA"
 MIN_F1=$(( ${#PRIMER_F1} * 2 / 3 ))
 MIN_R1=$(( ${#PRIMER_R1} * 2 / 3 ))
 MIN_LEN="30"
-THREADS="4"
 
 #CHECK AND EDIT THE ADDRESS FOR THE DEMULTIPLEXED SAMPLES.
 #for sequential structure:
 	#IF YOU ARE RUNNING SEQUENTIALLY IN AN UNIQUE FOLDER, USE:
 #for file in demulti_linked/*_LA.fq;
-for file in /cluster/projects/nn9813k/metapipe/ITS/demulti_linked/*_LA.fq; do pre="$(basename $file _LA.fq)"; echo $pre; cutadapt --quiet -j ${THREADS} -a file:Barcodes_LA1.fa -a file:Barcodes_LA2.fa -a file:Barcodes_LA3.fa --action trim --trim-n --max-n 0 --minimum-length ${MIN_LEN} -o ${pre}_trim1.fq --untrimmed-output ${pre}_bad_trim1.fq $file; done;
-
-
-for f in *_trim1.fq; do pre="$(basename $f _trim1.fq)"; echo $pre; cutadapt --quiet -j ${THREADS} -a "${PRIMER_F1}...${PRIMER_R1_RC}" -O "${MIN_F1}" -a "${PRIMER_R1}...${PRIMER_F1_RC}" -O "${MIN_F1}" --action=trim --minimum-length ${MIN_LEN} -o ${pre}_trim2.fq --untrimmed-output ${pre}_bad_trim2.fq $f; done;
-
+for file in /cluster/projects/nn9813k/metapipe/ITS/demulti_linked/*_LA.fq; do pre="$(basename $file _LA.fq)"; echo $pre; cutadapt --quiet -a file:Barcodes_LA1.fa -a file:Barcodes_LA2.fa -a file:Barcodes_LA3.fa --action trim --trim-n --max-n 0 --minimum-length ${MIN_LEN} -o ${pre}_trim1.fq --untrimmed-output ${pre}_bad_trim1.fq $file; done;
 mkdir adapter_clip
-mv *_trim1.fq adapter_clip/
+mkdir adapter_clip/BAD
+mv *_bad_trim1.fq adapter_clip/BAD
+mv *_trim1.fq adapter_clip
+
+for f in /adapter_clip/*_trim1.fq; do pre="$(basename $f _trim1.fq)"; echo $pre; cutadapt --quiet -a "${PRIMER_F1}...${PRIMER_R1_RC}" -O "${MIN_F1}" -a "${PRIMER_R1}...${PRIMER_F1_RC}" -O "${MIN_F1}" --action=trim --minimum-length ${MIN_LEN} -o ${pre}_trim2.fq --untrimmed-output ${pre}_bad_trim2.fq $f; done;
 mkdir primer_clip
+mkdir primer_clip/BAD
+mv *_bad_trim2.fq primer_clip/BAD
 mv *_trim2.fq primer_clip/
 
 
